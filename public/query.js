4b705d96f14b1e6c8ddac79e8d4d962a0a2945e4
nikhilNathwani <nnathwani36@gmail.com>
2024-07-19 16:35:32 -0400

    adding styling 6

--- public/query.js
+++ public/query.js
@@ -36,7 +36,7 @@ function makeWinLossTeamDiv(num_win, num_tie, num_loss) {
 	}`;
 
 	const percentSuccess = (100 * num_win) / (num_win + num_tie + num_loss);
-	caption.textContent = `${percentSuccess.toPrecision(3)}%`;
+	caption.textContent = `${percentSuccess.toPrecision(3)}% Win Rate`;
 }
 
 function makeWinLossIndividualDivs(titanRecords) {
