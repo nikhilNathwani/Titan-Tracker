039f924e918804ed26a559bc15f7b703da946e42
nikhilNathwani <nnathwani36@gmail.com>
2024-08-07 08:44:41 -0400

    script to automate data updates from new episodes 5

---
+++ titan_data/insertEpisode.sql
@@ -0,0 +1,5 @@
+
+                INSERT INTO titan_episodes (season_num, episode_num, challenger_name, judge_name)
+                VALUES 
+                (3, 3, 'Kevin Lee', 'Lucinda Scala Quinn');
+                
