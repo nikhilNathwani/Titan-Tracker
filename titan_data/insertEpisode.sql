9a2b1ad4aae802070eb98e6d4d82ab8b6eb75fa7
nikhilNathwani <nnathwani36@gmail.com>
2025-09-03 09:17:48 -0400

    including ayesha stats

--- titan_data/insertEpisode.sql
+++ titan_data/insertEpisode.sql
@@ -2,14 +2,21 @@
 ----------------------------------------------
 --  Insert new episode into titan_episodes  --
 ----------------------------------------------
-INSERT INTO titan_episodes (season_num, episode_num, challenger_name, judge_name)
-VALUES 
-(3, 4, 'Eric Adjepong', 'Chris Cheung');
+-- INSERT INTO titan_episodes (season_num, episode_num, challenger_name, judge_name)
+-- VALUES 
+-- (3, 4, 'Eric Adjepong', 'Chris Cheung');
 
+
+-- INSERT INTO titan_episodes (season_num, episode_num, challenger_name, judge_name)
+-- VALUES 
+-- (4, 1, 'Nini Nguyen', 'Daniel Boulud');
+
+
+SELECT * FROM titan_episodes;
 
 ----------------------------------------------
 --  Delete episode from titan_episodes      --
 ----------------------------------------------
-DELETE FROM titan_episodes
-WHERE season_num = 3
-AND episode_num = 4;        
+-- DELETE FROM titan_episodes
+-- WHERE season_num = 3
+-- AND episode_num = 4;        
--- titan_data/insertRounds.sql
+++ titan_data/insertRounds.sql
@@ -1,25 +1,34 @@
 ------------------------------------------
 --  Insert results into titan_rounds    --
 ------------------------------------------
-INSERT INTO titan_rounds (season_num, episode_num, round_num, titan_name, ingredient1, ingredient2, max_score, titan_score, challenger_score)
-VALUES 
-    (3, 4, 1, 'Michael Voltaggio', 'Rack of Lamb', 'Mint', 10, 7, 7),
-    (3, 4, 2, 'Tiffany Derry', 'Mussel', 'Fresno Chili', 10, 9, 6),
-    (3, 4, 3, 'Brooke Williamson', 'Goat Leg', 'Tamarind', 20, 18, 16);
+-- INSERT INTO titan_rounds (season_num, episode_num, round_num, titan_name, ingredient1, ingredient2, max_score, titan_score, challenger_score)
+-- VALUES 
+--     (3, 4, 1, 'Michael Voltaggio', 'Rack of Lamb', 'Mint', 10, 7, 7),
+--     (3, 4, 2, 'Tiffany Derry', 'Mussel', 'Fresno Chili', 10, 9, 6),
+--     (3, 4, 3, 'Brooke Williamson', 'Goat Leg', 'Tamarind', 20, 18, 16);
 
+
+-- INSERT INTO titan_rounds (season_num, episode_num, round_num, titan_name, ingredient1, ingredient2, max_score, titan_score, challenger_score)
+-- VALUES 
+--     (4, 1, 1, 'Brooke Williamson', 'Crawfish', 'Beef Tenderloin', 10, 8, 6),
+--     (4, 1, 2, 'Michael Voltaggio', 'Black Kale', 'Wild Mushroom', 10, 9, 8),
+--     (4, 1, 3, 'Ayesha Nurdjaja', 'Pork Shoulder', 'Rice Flour', 20, 15, 16);
+
+
+SELECT * FROM titan_rounds
 
 
 ------------------------------------------
 --  Delete results from titan_rounds    --
 ------------------------------------------
-DELETE FROM titan_rounds
-WHERE titan_name = 'Ayesha Nurdjaja';
+-- DELETE FROM titan_rounds
+-- WHERE titan_name = 'Ayesha Nurdjaja';
 
 
 
 ------------------------------------------
 --  Testing for Ayesha Nudjaja          --
 ------------------------------------------
-INSERT INTO titan_rounds (season_num, episode_num, round_num, titan_name, ingredient1, ingredient2, max_score, titan_score, challenger_score)
-VALUES 
-    (1, 1, 1, 'Ayesha Nurdjaja', 'Country Ham', 'Cheddar Cheese', 10, 9, 8);
+-- INSERT INTO titan_rounds (season_num, episode_num, round_num, titan_name, ingredient1, ingredient2, max_score, titan_score, challenger_score)
+-- VALUES 
+--     (1, 1, 1, 'Ayesha Nurdjaja', 'Country Ham', 'Cheddar Cheese', 10, 9, 8);
