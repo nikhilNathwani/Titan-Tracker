692222a64e8a475a4ec32b88cc846c23fed7c1d1
nikhilNathwani <nnathwani36@gmail.com>
2025-10-09 17:39:38 -0400

    added updateTitans sql file if any titans are replaced in the future. and cleaned up all sql files, formatted them consistenly

--- data/insertEpisode.sql
+++ data/insertEpisode.sql
@@ -1,18 +1,23 @@
+-- Table name: titan_episodes 
+-- Schema:
+--      season_num          INTEGER NOT NULL
+--      episode_num         INTEGER NOT NULL
+--      challenger_name     VARCHAR NOT NULL
+--      judge_name          VARCHAR NOT NULL
+--      PRIMARY KEY         (season_num, episode_num)
 
-----------------------------------------------
---  Insert new episode into titan_episodes  --
-----------------------------------------------
 
+-- Query 1: View all episodes
+SELECT * FROM titan_episodes;
+
+
+-- Query 2: Insert new episode into titan_episodes
 -- INSERT INTO titan_episodes (season_num, episode_num, challenger_name, judge_name)
 -- VALUES 
 -- (4, 6, 'Martel Stone', 'Lorna Maseko');
 
- 
-SELECT * FROM titan_episodes;
 
-----------------------------------------------
---  Delete episode from titan_episodes      --
-----------------------------------------------
+-- Query 3: Delete episode from titan_episodes (use with caution)
 -- DELETE FROM titan_episodes
 -- WHERE season_num = 3
 -- AND episode_num = 4;        
--- data/insertRounds.sql
+++ data/insertRounds.sql
@@ -1,7 +1,22 @@
-------------------------------------------
---  Insert results into titan_rounds    --
-------------------------------------------
+-- Table name: titan_rounds 
+-- Schema:
+--      season_num          INTEGER NOT NULL
+--      episode_num         INTEGER NOT NULL
+--      round_num           INTEGER NOT NULL (1, 2, or 3)
+--      titan_name          VARCHAR NOT NULL (REFERENCES titans.titan_name)
+--      ingredient1         VARCHAR NOT NULL
+--      ingredient2         VARCHAR NOT NULL
+--      max_score           INTEGER NOT NULL (10 for rounds 1&2, 20 for round 3)
+--      titan_score         INTEGER NOT NULL
+--      challenger_score    INTEGER NOT NULL
+--      PRIMARY KEY         (season_num, episode_num, round_num)
 
+
+-- Query 1: View all rounds
+SELECT * FROM titan_rounds;
+
+
+-- Query 2: Insert results into titan_rounds
 -- INSERT INTO titan_rounds (
 --     season_num, episode_num, round_num, titan_name, 
 --     ingredient1, ingredient2, 
@@ -16,20 +31,6 @@
 --     'Suya Spice', 'Gooseberry', 20, 16, 18);
 
 
-SELECT * FROM titan_rounds
-
-
-------------------------------------------
---  Delete results from titan_rounds    --
-------------------------------------------
+-- Query 3: Delete results from titan_rounds (use with caution)
 -- DELETE FROM titan_rounds
 -- WHERE season_num = 4 and episode_num = 5;
-
-
-
-------------------------------------------
---  Testing for Ayesha Nudjaja          --
-------------------------------------------
--- INSERT INTO titan_rounds (season_num, episode_num, round_num, titan_name, ingredient1, ingredient2, max_score, titan_score, challenger_score)
--- VALUES 
---     (1, 1, 1, 'Ayesha Nurdjaja', 'Country Ham', 'Cheddar Cheese', 10, 9, 8);
