de78b28d2ff1a84ef43994f244c05c9c527d513b
nikhilNathwani <nnathwani36@gmail.com>
2026-01-25 15:21:23 -0500

    sql window function for titan tanks

--- app/queries/records/titanRecords.sql
+++ app/queries/records/titanRecords.sql
@@ -18,20 +18,33 @@ WITH titan_stats AS (
 		COUNT(CASE WHEN titan_score < challenger_score THEN 1 END) AS num_loss
 	FROM titan_rounds
 	GROUP BY titan_name
+),
+titan_scores AS (
+	SELECT
+		titan_name,
+		num_win,
+		num_tie,
+		num_loss,
+		CASE 
+			WHEN (num_win + num_loss) = 0 THEN 0
+			ELSE num_win::float / (num_win + num_loss)
+		END AS score
+	FROM titan_stats
 )
 SELECT
 	ts.titan_name,
+	t.is_retired,
+	RANK() OVER (
+		PARTITION BY t.is_retired
+		ORDER BY
+			ts.score DESC
+	) AS rank,
 	ts.num_win,
 	ts.num_tie,
-	ts.num_loss,
-	CASE 
-		WHEN (ts.num_win + ts.num_loss) = 0 THEN 0
-		ELSE ts.num_win::float / (ts.num_win + ts.num_loss)
-	END AS score,
-	t.is_retired
-FROM titan_stats ts
+	ts.num_loss
+FROM titan_scores ts
 JOIN titans t ON ts.titan_name = t.titan_name
 ORDER BY
 	t.is_retired ASC,  -- active (false) first
-	score DESC,        -- highest score
-	ts.titan_name ASC; -- alphabetical
+	rank ASC,          -- highest score
+	titan_name ASC;    -- alphabetical
