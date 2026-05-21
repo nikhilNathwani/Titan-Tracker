10927ec3701caf77184acb88227249fc452f6f9a
nikhilNathwani <nnathwani36@gmail.com>
2026-05-21 17:29:06 -0400

    Fix footer card styling; fix header alignment; update share text; desktop share uses clipboard

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
 	return (
-		<div className={`section ${styles.footer}`} id="footer">
-			<div className={`section-content ${styles.content}`}>
+		<div className={styles.footer} id="footer">
+			<div className={styles.content}>
 				<p>Made by fan of the show, Nikhil N.</p>
 				<div className={styles.links}>
 					<a
--- components/TitanCard.jsx
+++ components/TitanCard.jsx
@@ -38,7 +38,7 @@ export default function TitanCard({
 				<ShareButton
 					sectionId={titanId}
 					sectionName={titan.titan_name}
-					shareText={`Check out ${titan.titan_name}'s stats on Titan Tracker`}
+					shareText={`Check out ${titan.titan_name}'s stats for Bobby's Triple Threat on Food Network`}
 				/>
 				{/* Header: avatar + rank badge + name + win rate */}
 				<div className={styles.header}>
