f67aa7344c61898416e7f1c91f23401289c89281
nikhilNathwani <nnathwani36@gmail.com>
2026-05-21 18:05:04 -0400

    Footer: let the link reveal the name, simplify text to 'Made by a fan of the show'

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<div className={styles.footer} id="footer">
 			<div className={styles.content}>
-				<p>Made by fan of the show, Nikhil N.</p>
+				<p>Made by a fan of the show</p>
 				<div className={styles.links}>
 					<a
 						href="https://nikhilnathwani.com"
